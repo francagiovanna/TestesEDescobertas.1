95a876b9e7952d980e15fce0b983cfbd0a04b7e9
francagiovanna <francamariagiovanna4@gmail.com>
2025-05-26 23:37:04 -0300

    Emplementos HTML

--- exercicio.sala/script.js
+++
@@ -1,8 +0,0 @@
-function mostrarLogin(){
-    document.getElementById('login').style.display = 'flex'
-    document.getElementById('cadastro').style.display = 'none'
-}
-function mostrarCadastro(){
-    document.getElementById('login').style.display = 'none'
-    document.getElementById('cadastro').style.display = 'flex'
-}
